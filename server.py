67c6048da7b49b4f1a06304883eefaad5c01e4f3
Elephant_logic <keith.davies1978@gmail.com>
2026-08-05 16:20:06 +0100

    Load authoritative recipe conversion patch without cached script

--- server.py
+++ server.py
@@ -100,7 +100,7 @@ class Handler(app.Handler):
                 '<script src="/recipe_menu_patch.js?v=1"></script>'
                 '<script src="/recipe_management_patch.js?v=1"></script>'
                 '<script src="/prep_delete_patch.js?v=1"></script>'
-                '<script src="/single_dish_cleanup_patch.js?v=1"></script>'
+                '<script src="/single_dish_cleanup_patch.js?v=2"></script>'
                 '<script src="/menu_builder_patch.js?v=1"></script>'
                 '<script src="/menu_photo_import_patch.js?v=1"></script>'
                 '<script src="/clockin_session_patch.js?v=1"></script>'
